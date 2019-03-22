1ef6b1571f1ae85650c7f1b4d0e4d06e97124c1b
mikhailovpn <37391961+mikhailovpn@users.noreply.github.com>
2019-03-22 20:40:16 +0800

    Modal-window in progress

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-var sendButton = document.querySelector('.sendButton');
-var modalFeedback = document.querySelector('.modal-feedback');
-var modalClose = document.querySelector('.modal-close');
-
-sendButton.onclick = openModalFeedback;
-modalClose.onclick = closeModalFeedback;
+var sendButton, modalFeedback, modalClose;
 
 function openModalFeedback() {
 	modalFeedback.setAttribute('class', 'modal-feedback unhidden');
+	return false;
 }
 
 function closeModalFeedback() {
-	modalFeedback.setAttribute('class', 'modal-feedback')
+	modalFeedback.setAttribute('class', 'modal-feedback hidden')
+	console.log()
+	return false;
+}
+
+function $(selector){
+	return document.querySelector(selector);
+} 
+	
+function onLoad(){
+
+     //sendButton = document.querySelector('.sendButton');
+		 sendButton = $('.sendButton');
+     modalFeedback = document.querySelector('.modal-feedback');
+     modalClose = document.querySelector('.modal-close');
+
+sendButton.onclick = openModalFeedback;
+modalClose.onclick = closeModalFeedback;
 }
