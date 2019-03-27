d35452a7d569beb4ec3099dc72edb5520fbcafdc
mikhailovpn <37391961+mikhailovpn@users.noreply.github.com>
2019-03-27 20:37:55 +0800

    JS in progress

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var sendButton, modalFeedback, modalClose, slideButtons;
+var sendButton, modalFeedback, modalClose, slideItems, slideButtons, activeSlideItem, activeSlideButton;
 
 function openModalFeedback() {
 		modalFeedback.classList.remove('hidden');
@@ -20,22 +20,46 @@ function $$(selector){
 }
 
 function slide(target) {
-	console.log(target);
-}
+		console.log(target);
+} 
+
+function activateSlide(event) {
+	
+	//1 получение номера
+	//1.1 на лету
+
+	for (i = 0; i <  slideButtons.length; i++) {
+		if (activeSlideButton = slideButtons[i]) {
+				slideButtons[i].classList.remove('js-slider-button-active');
+				slideItems[i].classList.remove('js-slider-item-active');
+		}
+	}
+	
+	//1.2 через данные
+	
+	//2 отображение iго слайда
+	//1.1 на лету
+	//1.2 через данные
+	
+	
+}	
 
 function onLoad(){
 		sendButton = $('.sendButton');
 		modalFeedback = $('.modal-feedback');
 		modalClose = $('.modal-close');
+		activeSlideItem = $('.js-slider-item-active');
+		activeSlideButton = $('.js-slider-button-active');
+		slideItems = $$('.js-slider-item');
 		slideButtons = $$('.js-slider-button');
+		
+		
 
 		sendButton.onclick = openModalFeedback;
 		modalClose.onclick = closeModalFeedback;
 
-
-
-		for(var key in slideButtons) {
-			slideButtons[key].onclick = slide;
-			
+		for (var i = 0; i < slideItems.length; i++) {
+				slideButtons[i].onclick = activateSlide;
+					
 		}
 }
